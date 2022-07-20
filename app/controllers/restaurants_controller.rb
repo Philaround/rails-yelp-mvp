26c33639517d8471a5dbd63c2c64f942be563f9f
Filippos Lavrentidis <n.p.lavrentidis@gmail.com>
2022-07-20 20:03:43 +0300

    created controllers and initial actions

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,3 +1,16 @@
 class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(params[:restaurant])
+    @restaurant.save
+    # Will raise ActiveModel::ForbiddenAttributesError
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+o_stavros = { name: 'O Stavros', address: 'Eratosthenous 12', category: 'Japanese' }
+musique_cafe =  { name: 'Musique Cafe', address: 'Plateia Proskopon 12', category: 'french' }
+black_sheep = { name: 'Black Sheep', address: 'Arktinou 12', category: 'chinese' }
+ok_bread = { name: 'Ok Bread', address: 'Aminta 8', category: 'Belgian' }
+aleksandrino = { name: 'Aleksandrino', address: 'Imitou 12', category: 'italian' }
+
+[o_stavros, musique_cafe, black_sheep, ok_bread, aleksandrino].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
