b2b658055db782f1f63d3a9c4a0f54ce4e6e042a
Filippos Lavrentidis <n.p.lavrentidis@gmail.com>
2022-07-21 20:50:23 +0300

    reviews portion done

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/restaurants', to: 'restaurants#index'
-
-  post 'restaurants', to: 'restaurants#create'
-  get 'restaurants/new', to: 'restaurants#new'
-
-  get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
-
-  
-
-
-
+  resources :restaurants, only: [:index, :create, :new, :show] do
+    resources :reviews, only: [:new, :create]
+  end
 end
+
+
+
+
+  # get '/restaurants', to: 'restaurants#index'
+
+  # post 'restaurants', to: 'restaurants#create'
+  # get 'restaurants/new', to: 'restaurants#new'
+
+  # get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
