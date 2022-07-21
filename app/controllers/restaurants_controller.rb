fadda615b91f8a3058d4ee0bb4c3bb90f6902b4f
Filippos Lavrentidis <n.p.lavrentidis@gmail.com>
2022-07-21 17:03:27 +0300

    index html added for testing

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -3,14 +3,6 @@ class RestaurantsController < ApplicationController
     @restaurants = Restaurant.all
   end
 
-  def new
-    @restaurant = Restaurant.new
-  end
 
-  def create
-    @restaurant = Restaurant.new(params[:restaurant])
-    @restaurant.save
-    # Will raise ActiveModel::ForbiddenAttributesError
-  end
 
 end
